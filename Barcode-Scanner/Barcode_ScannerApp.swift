9b1ab9c32f76c7f667eefb486d8c5d1bfa66a859
davidcafor <davidcafor@gmail.com>
2024-04-17 17:34:51 +0200

    Initial commit

---
+++ Barcode-Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode-Scanner
+//
+//  Created by David Cardeña on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
