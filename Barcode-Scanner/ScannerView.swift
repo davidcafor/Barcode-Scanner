aef6ec7c1edddba909eb89f0bdf6b30cf60d9562
davidcafor <davidcafor@gmail.com>
2024-04-17 19:40:58 +0200

    Views & ViewModel

---
+++ Barcode-Scanner/ScannerView.swift
@@ -0,0 +1,52 @@
+//
+//  ScannerView.swift
+//  Barcode-Scanner
+//
+//  Created by David Cardeña on 17/4/24.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    
+    @Binding var scannedCode: String
+    @Binding var alertItem: AlertItem?
+        
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(scannerView: self)
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        
+        private let scannerView: ScannerView
+        
+        init(scannerView: ScannerView) {
+            self.scannerView = scannerView
+        }
+        
+        func didFind(barcode: String) {
+            scannerView.scannedCode = barcode
+        }
+        
+        func didSurface(error: CameraError) {
+            switch error {
+            case .invalidDeviceInput:
+                scannerView.alertItem = AlertContext.invalidDeviceInput
+            case .invalidScannValue:
+                scannerView.alertItem = AlertContext.invalidScannedType
+            }
+        }
+        
+        
+    }
+}
+
+#Preview {
+    ScannerView(scannedCode: .constant("00111"), alertItem: .constant(AlertItem(title: "a", message: "b", dismissButton: .default(Text("c")))))
+}
